d9833802f33c95f39517ad0d1e1f70bc50769264
abhishekojha72 <ojhah96@gmail.com>
2025-06-26 21:16:46 +0530

    Fix ViewPaste fallback and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,83 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import Home from "./Components/Home"
+// import Paste from "./Components/Paste"
+// import ViewPaste from "./Components/ViewPaste"
+// import Navbar from "./Components/Navbar"
+
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path:"/",
+//       element:
+//       <div className="w-full h-full flex flex-col">
+//         <Navbar/>
+//         <Home/>
+//       </div>
+//     },
+//     {
+//       path:"/pastes",
+//       element: <div className="w-full h-full flex flex-col">
+//       <Navbar/>
+//       <Paste/>
+//     </div>
+//     },
+//     {
+//       path:"/pastes/:id",
+//       element: <div className="w-full h-full flex flex-col">
+//       <Navbar/>
+//       <ViewPaste/>
+//     </div>,
+//     }
+//   ]
+// )
+
+//    function App() {
+//     return (
+//      <RouterProvider router={router} />
+
+//    );
+//  }
+
+// export default App;
+
+import { createHashRouter, RouterProvider } from "react-router-dom"
 import Home from "./Components/Home"
 import Paste from "./Components/Paste"
 import ViewPaste from "./Components/ViewPaste"
 import Navbar from "./Components/Navbar"
 
-const router = createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:
+const router = createHashRouter([
+  {
+    path: "/",
+    element: (
       <div className="w-full h-full flex flex-col">
-        <Navbar/>
-        <Home/>
+        <Navbar />
+        <Home />
       </div>
-    },
-    {
-      path:"/pastes",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <Paste/>
-    </div>
-    },
-    {
-      path:"/pastes/:id",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <ViewPaste/>
-    </div>,
-    }
-  ]
-)
+    ),
+  },
+  {
+    path: "/pastes",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <Paste />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes/:id",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <ViewPaste />
+      </div>
+    ),
+  },
+]);
 
-   function App() {
-    return (
-     <RouterProvider router={router} />
-
-   );
- }
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
-
